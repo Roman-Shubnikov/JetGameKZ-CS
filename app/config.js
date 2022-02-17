29ef7c8acc8efc3d6bd34af74a10633c91e44f72
Roman <62247180+Roman-Shubnikov@users.noreply.github.com>
2022-02-17 18:38:43 +0300

    fix

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const API_URL = dev ? 'http://localhost:3000/api' : 'http://localhost:3000/api';
+export const API_URL = dev ? 'http://localhost:3000/api' : 'https://jet-game-kz-cs.vercel.app/api';
 export const SOCIAL_MEDIA = {
     vk: 'https://vk.com/jetgame',
     telegram: 'https://t.me/kz_jetgame',
